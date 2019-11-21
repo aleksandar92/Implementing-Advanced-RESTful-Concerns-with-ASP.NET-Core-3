1a821fbd8ed779441595e95665c97f2b89298a74
Jose Manuel Campos Soberano <jmcsoberano@hotmail.com>
2019-11-21 09:55:06 -0600

    Finish

--- CourseLibrary.API/ResourceParameters/AuthorsResourceParameters.cs
+++ CourseLibrary.API/ResourceParameters/AuthorsResourceParameters.cs
@@ -17,7 +17,5 @@ namespace CourseLibrary.API.ResourceParameters
             get => _pageSize; 
             set => _pageSize = (value > maxPageSize) ? maxPageSize : value; 
         }
-
-        public string OrderBy { get; set; } = "Name";
     }
 }
