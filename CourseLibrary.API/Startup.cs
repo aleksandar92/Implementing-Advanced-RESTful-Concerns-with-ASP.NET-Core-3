362005178f858dec9d191baacbc510023c35c49c
Jose Manuel Campos Soberano <jmcsoberano@gmail.com>
2019-12-05 15:38:13 -0600

    Finish

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -32,6 +32,8 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpCacheHeaders();
+
             services.AddResponseCaching();
 
             services.AddControllers(setupAction =>
@@ -121,6 +123,8 @@ namespace CourseLibrary.API
             //app.UseHttpsRedirection();
 
             app.UseResponseCaching();
+
+            app.UseHttpCacheHeaders();
 
             app.UseRouting();
 
